c8d4e9140797e92a1f5bbcec41de6754bf0a9402
webdev730 <webdevstarjus730@gmail.com>
2019-02-02 17:46:33 -0500

    .

--- script.sh
+++
@@ -1,4 +0,0 @@
-git remote remove origin
-git remote add origin https://github.com/webdev730/webdev730_socialnetwork.git
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='WebDevStar730'; GIT_AUTHOR_EMAIL='webdevstarjus730@gmail.com'; GIT_COMMITTER_NAME='WebDevStar730'; GIT_COMMITTER_EMAIL='webdevstarjus730@gmail.com';" HEAD;
-git push --force --tags origin 'refs/heads/*'
